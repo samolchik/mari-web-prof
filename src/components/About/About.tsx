52894670e98f11fee513156fcb77fe973ca9dd53
DvlprMrn <rplc.999@outlook.com>
2023-07-06 21:24:18 -0400

    commit 3

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,13 +5,15 @@ import photo from '../../assets/photomain.png';
 const About: React.FC = () => {
     return (
         <section id="about" className="about-section">
+            <div className="about-container">
             <div className="photo-container">
                 <img src={photo} alt="My Photo" />
             </div>
 
-            <div className="container">
+            <div className="about-me">
                 <h2>Hi, I'm Marina.</h2>
                 <p>Full Stack Web Developer</p>
+            </div>
             </div>
         </section>
     );
