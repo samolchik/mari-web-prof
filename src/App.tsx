ab0e27fe84174c9c4a0ffc5c74e026c4f169f9a3
DvlprMrn <rplc.999@outlook.com>
2023-07-08 15:30:54 -0400

    commit 3

--- src/App.tsx
+++ src/App.tsx
@@ -30,26 +30,3 @@ const App: React.FC = () => {
 
 export default App;
 
-
-
-// import { Route, Routes } from 'react-router-dom';
-// import { Home, MovieListing, MovieDetails, MoviesByGenre, Genres, PageNotFound, Header, Footer } from './components';
-//
-// import './App.css';
-//
-// const App: React.FC = () => {
-//     return (
-//         <div className="App">
-//             <Header />
-//             <Routes>
-//                 <Route path="/" element={<Home />} />
-//                 <Route path="/movie-listing" element={<MovieListing />} />
-//                 <Route path="/movie-details/:movieId" element={<MovieDetails />} />
-//                 <Route path="/movies/genre/:genreId" element={<MoviesByGenre />} />
-//                 <Route path="/genres" element={<Genres />} />
-//                 <Route path="/*" element={<PageNotFound />} />
-//             </Routes>
-//             <Footer />
-//         </div>
-//     );
-// };
