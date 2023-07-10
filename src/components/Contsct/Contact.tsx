4cb9b79473de36839269521fe1babfdf2c262df7
DvlprMrn <rplc.999@outlook.com>
2023-07-10 02:36:22 -0400

    commit 3

--- src/components/Contsct/Contact.tsx
+++ src/components/Contsct/Contact.tsx
@@ -1,5 +1,5 @@
 import React, { useState, ChangeEvent, FormEvent } from 'react';
-import './Contsct.css';
+import './Contact.css';
 
 const Contact = () => {
     const [name, setName] = useState('');
