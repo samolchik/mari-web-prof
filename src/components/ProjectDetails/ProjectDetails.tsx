f80af00b77ff8bc376667d49791261320111b3b1
DvlprMrn <rplc.999@outlook.com>
2023-07-15 16:40:12 -0400

    commit 3

--- src/components/ProjectDetails/ProjectDetails.tsx
+++ src/components/ProjectDetails/ProjectDetails.tsx
@@ -10,9 +10,11 @@ const ProjectDetails: React.FC<ProjectDetailsProps> = ({ onClose }) => {
     return (
         <div className="details-wrapper">
         <div className="details">
+            <div className="border">
             <div className="close-button">
             <button className="close" onClick={onClose}>Close</button>
             </div>
+        </div>
         </div>
         </div>
     );
