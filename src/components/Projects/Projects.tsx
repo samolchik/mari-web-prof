5c591c4bcf37c0bbdafb60f7115bb0ede292dce1
DvlprMrn <rplc.999@outlook.com>
2023-08-17 21:30:36 -0400

    added project 2

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -14,7 +14,14 @@ import screen9 from '../../assets/screenE3.jpeg';
 import screen10 from '../../assets/screenE4.jpeg';
 import screen11 from '../../assets/screenE5.jpeg';
 import screen12 from '../../assets/screenE6.jpeg';
+import screen13 from '../../assets/screenCar1.jpeg';
+import screen14 from '../../assets/screenCar2.jpeg';
+import screen15 from '../../assets/screenCar3.jpeg';
+import screen16 from '../../assets/screenCar4.jpeg';
+import screen17 from '../../assets/screenCar5.jpeg';
+import screen18 from '../../assets/screenCar6.jpeg';
 import furniture from '../../assets/furniturehome.jpeg';
+import cars from '../../assets/carshome.jpeg';
 
 const Projects = () => {
     const [selectedProject, setSelectedProject] = useState<string | null>(null);
@@ -87,6 +94,34 @@ const Projects = () => {
                         </div>
                     </div>
                 </div>
+                <div className="project1">
+                    <div className="homePage">
+                        <img
+                            src={cars}
+                            alt="Cars"
+                            onClick={() => handleClickProject('cars')}
+                        />
+                    </div>
+                    <div className="project-description">
+                        <h1>Car Sale Platform Back End</h1>
+                        <div className="description">
+                            <div>
+                                <p>Technologies used:</p>
+                            </div>
+                            <ul>
+                                <li>Node.js</li>
+                                <li>Express.js</li>
+                                <li>EJS</li>
+                                <li>MongoDB</li>
+                                <li>Typescript</li>
+                                <li>JWT</li>
+                                <li>Git</li>
+                                <li>NPM</li>
+                            </ul>
+                            <div className="findOut">Click on the image to find out more</div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             {selectedProject === 'furniture' && (
@@ -106,13 +141,26 @@ const Projects = () => {
                 <ProjectDetails
                     onClose={handleCloseProjectDetails}
                     images={[ screen6, screen1, screen3,  screen4, screen2, screen5]}
-                    projectDescription="Hey there! Check out the tech behind my Movie Search project."
+                    projectDescription="Explore the Tech Behind My Movie Search Project!"
                     projectTechnologies={['React: I utilized the power of React, a leading JavaScript library, to create a smooth and interactive user interface. The website offers an immersive experience while exploring movies.',
                         'TypeScript: Boosting code reliability, I integrated TypeScript for a more efficient and readable codebase.',
                         'Redux Toolkit:Mastering complex states was a breeze with Redux Toolkit, ensuring a captivating user journey through seamless movie exploration.',
                         'TMDb API Integration:I integrated the TMDb API, granting real-time access to a vast collection of movie data.']}
                     websiteLink="https://movie-search-opal-two.vercel.app/"
                     githubLink="https://github.com/DevMari999/movie_search"
+                />
+            )}
+            {selectedProject === 'cars' && (
+                <ProjectDetails
+                    onClose={handleCloseProjectDetails}
+                    images={[ screen13, screen14, screen15,  screen16, screen17, screen18]}
+                    projectDescription="Hey there! Explore the tech behind my Car Sale project."
+                    projectTechnologies={['Node.js: At the core of this project lies Node.js, an industry-leading runtime that ensures the application operates with performance in mind.',
+                        'Express: Leaning on the minimalism and flexibility of Express.js, the application is not only agile but also easily maintainable. This lightweight framework has streamlined the creation of API endpoints, making data retrieval both swift and reliable.',
+                        'JWT (JSON Web Tokens): With security at the forefront, JWT has been integrated for top-notch authentication and authorization. This assures that data communications remain secure and trustworthy',
+                        'EJS (Embedded JavaScript Templates): Utilizing EJS facilitates server-side templating, offering dynamic content rendering capabilities. This ensures that content can be tailored and presented based on specific requirements and conditions.']}
+                    websiteLink="https://car-sale-platform-c3fa3b2c4642.herokuapp.com"
+                    githubLink="https://github.com/DevMari999/backend_project"
                 />
             )}
         </section>
