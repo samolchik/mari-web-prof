f2b6b5be142ab6e885a669c738d0e64b4522c47b
DvlprMrn <rplc.999@outlook.com>
2023-09-16 19:36:05 -0400

    details

--- src/components/ProjectDetails/ProjectDetails.tsx
+++ src/components/ProjectDetails/ProjectDetails.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './ProjectDetails.css';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 interface ProjectDetailsProps {
     onClose: () => void;
