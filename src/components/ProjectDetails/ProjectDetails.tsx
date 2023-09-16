ffd3dcbf6bcba4084bee40a7d9d9834873cbf42a
DvlprMrn <rplc.999@outlook.com>
2023-09-16 19:17:35 -0400

    details

--- src/components/ProjectDetails/ProjectDetails.tsx
+++ src/components/ProjectDetails/ProjectDetails.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import Slider from 'react-simple-image-slider';
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './ProjectDetails.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 interface ProjectDetailsProps {
     onClose: () => void;
@@ -17,67 +19,62 @@ const ProjectDetails: React.FC<ProjectDetailsProps> = ({
                                                            projectDescription,
                                                            projectTechnologies,
                                                            websiteLink,
-                                                           githubLink,
-                                                       }) => {
-    const sliderImages = images.map((image) => ({url: image}));
-
+                                                           githubLink}) => {
     return (
         <div className="background">
-            <div className="details-wrapper">
-                <div className="details">
-                    <div className="close-button">
-                        <button className="close" onClick={onClose}>
-                            CLOSE
+            <div className="screen-shots">
+                <div className="close-button">
+                    <button className="close" onClick={onClose}>
+                        CLOSE
+                    </button>
+                </div>
+                <div className="slider-container">
+                    <div className="slider-wrapper">
+                        <Carousel
+                            autoPlay={true}
+                            interval={3500}
+                            infiniteLoop={true}
+                            dynamicHeight={true}
+                            stopOnHover={true}
+                        >
+                            {images.map((image, index) => (
+                                <div key={index}>
+                                    <img src={image} alt={`screenshot ${index + 1}`} />
+                                </div>
+                            ))}
+                        </Carousel>
+                    </div>
+                </div>
+                <div className="description-details">
+                    <p>{projectDescription}</p>
+                    <ul>
+                        {projectTechnologies.map((tech, index) => (
+                            <li key={index}>
+                                {tech.includes(':') ? (
+                                    <>
+                                        <span className="highlight">
+                                            {tech.substring(0, tech.indexOf(':') + 1)}
+                                        </span>
+                                        {tech.substring(tech.indexOf(':') + 1)}
+                                    </>
+                                ) : (
+                                    tech
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="contact-button-div">
+                    <a href={websiteLink} target="_blank" rel="noopener noreferrer">
+                        <button className="contact-button description-details-button">
+                            GO TO WEBSITE
                         </button>
-                    </div>
-                    <div className="screen-shots">
-                        <div className="slider-container">
-                            <div className="slider-wrapper">
-                                <Slider  images={sliderImages}
-                                         width={600}
-                                         height={395}
-                                         showNavs={true}
-                                         showBullets={false}
-                                         navMargin={-5}
-                                         autoPlay={true}
-                                         navSize={30}
-                                         autoPlayDelay={3}/>
-
-                            </div>
-                        </div>
-                        <div className="description-details">
-                            <p>{projectDescription}</p>
-                            <ul>
-                                {projectTechnologies.map((tech, index) => (
-                                    <li key={index}>
-                                        {tech.includes(':') ? (
-                                            <>
-                        <span className="highlight">
-                          {tech.substring(0, tech.indexOf(':') + 1)}
-                        </span>
-                                                {tech.substring(tech.indexOf(':') + 1)}
-                                            </>
-                                        ) : (
-                                            tech
-                                        )}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-
-                        <div className="contact-button-div">
-                            <a href={websiteLink} target="_blank" rel="noopener noreferrer">
-                                <button className="contact-button description-details-button">
-                                    GO TO WEBSITE
-                                </button>
-                            </a>
-                            <a href={githubLink} target="_blank" rel="noopener noreferrer">
-                                <button className="contact-button description-details-button">
-                                    GO TO GITHUB
-                                </button>
-                            </a>
-                        </div>
-                    </div>
+                    </a>
+                    <a href={githubLink} target="_blank" rel="noopener noreferrer">
+                        <button className="contact-button description-details-button">
+                            GO TO GITHUB
+                        </button>
+                    </a>
                 </div>
             </div>
         </div>
